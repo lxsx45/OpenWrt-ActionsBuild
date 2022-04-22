32e12cc52d562218723eb3631f092c06b47bb4ff
lxsx45 <56106944+lxsx45@users.noreply.github.com>
2022-04-22 08:29:31 +0800

    🥳

--- XIAOMI-CR660X/make_befor.sh
+++ XIAOMI-CR660X/make_befor.sh
@@ -10,7 +10,7 @@ cp /usr/bin/upx-ucl /workdir/openwrt/staging_dir/host/bin/
 
 ls
 #修改后台IP
-sed -i 's/192.168.1.1/192.168.32.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 #修改默认主题(lede暂不需要)
 #cd ./package/
 #git clone https://github.com/jerrykuku/luci-theme-argon.git
